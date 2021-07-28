0dab1660f3b5733318500d91bae8b62967951020
YonghyeonKim <kyh1027@hanmail.net>
2021-07-28 16:45:01 +0900

    asdf

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -30,5 +29,6 @@ public class JpaMain {
             em.close();
         }
         emf.close();
+//112112
     }
 }
